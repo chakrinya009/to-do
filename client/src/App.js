76200a9487df3f2f241b1c759b2dc694a4a1a653
chakrinya <chakrinyabalabadra@gmail.com>
2023-07-22 12:00:08 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./components/Header";
+import "./App.css";
+import Todoform from "./components/Todoform";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Todoform />
+    </div>
+  );
+};
+
+export default App;
