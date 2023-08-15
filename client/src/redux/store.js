89c5de195142b2fe15f1d6bc7b8ccb9ffe5b9ed6
chakrinya <chakrinyabalabadra@gmail.com>
2023-08-15 14:05:35 +0530

    store

---
+++ client/src/redux/store.js
@@ -0,0 +1,16 @@
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { todosReducer } from "./reducers/todosReducer";
+const reducer = combineReducers({
+  todosReducer,
+});
+
+const middleware = [thunk];
+
+const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
